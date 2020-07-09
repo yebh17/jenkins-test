a64408f890daebf5b4538ba369d0682e01f0a9a8
Ubuntu <ubuntu@ip-172-31-39-73.eu-north-1.compute.internal>
2020-07-09 20:34:37 +0000

    small changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/hello/')
 def hello_world():
-    return 'Hello World!\n'
+    return 'Hello World This is bharani!\n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')     # open for everyone
--- test.py
+++ test.py
@@ -11,12 +11,12 @@ class TestHello(unittest.TestCase):
     def test_hello(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        self.assertEqual(rv.data, b'Hello World This is bharani!\n')
 
     def test_hello_hello(self):
         rv = self.app.get('/hello/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        self.assertEqual(rv.data, b'Hello World This is bharani!\n')
 
 if __name__ == '__main__':
     unittest.main()
